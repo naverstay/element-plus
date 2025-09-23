6ec33c3ed41003d0e19d138f41b90e022c64acc4
Dmitry <naverstay.me@gmail.com>
2025-09-23 13:23:56 +0200

    feat(utils): generate-package-publish.js

---
+++ generate-package-publish.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const path = require('path')
+
+const inputPath = path.resolve(__dirname, 'package.json')
+const outputPath = path.resolve(__dirname, 'dist', 'package.publish.json')
+
+const pkg = JSON.parse(fs.readFileSync(inputPath, 'utf-8'))
+
+const allowedFields = [
+  'name',
+  'version',
+  'description',
+  'license',
+  'main',
+  'module',
+  'types',
+  'typings',
+  'files',
+  'dependencies',
+  'peerDependencies',
+  'sideEffects',
+  'engines',
+  'exports',
+]
+
+const publishPkg = {}
+for (const key of allowedFields) {
+  if (pkg[key] !== undefined) {
+    publishPkg[key] = pkg[key]
+  }
+}
+
+if (!fs.existsSync(path.dirname(outputPath))) {
+  fs.mkdirSync(path.dirname(outputPath), { recursive: true })
+}
+
+fs.writeFileSync(outputPath, JSON.stringify(publishPkg, null, 2))
+
+console.log(`package.publish.json created in ${outputPath}`)
